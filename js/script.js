e8bb4409e24943884e0c8274bb65ab4c3634f3bc
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-02-08 23:45:49 +0100

    Creating carousel

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//JS
+$(function(){ // czekanie na załadowanie DOM
+  var carouselList = $("#carousel ul"); // pobieramy listę elementów i przypisujemy do zmiennej
+	var timeChangeSlides = setInterval(function(){ // funkcja 'setInterval' jest wykonywana co pewien okres czasu (a nie tylko raz po pewnym okresie czasu).
+		carouselList.animate({marginLeft:-800}, 4000, function(){ // animacja/właściwości css animowanego elementu/czas animacji/funkcja która wykona się na końcu animacji ('callback') = animujemy wartość lewego marginesu.
+			var firstItem = carouselList.find("li:first");
+      var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+      // $(this).find("li:last").after($(this).find("li:first")); - również działa zamiast :6 :7 :8
+			  carouselList.css({marginLeft:0});
+		});
+  });
+});
