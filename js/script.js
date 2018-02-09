f81bf3e15422a4b7dae146de129bd40fcd3da0cd
Dawid Kozakiewicz <turgor1996@gmail.com>
2018-02-10 00:07:22 +0100

    Preparing files

--- js/script.js
+++ js/script.js
@@ -1,13 +1,15 @@
-//JS
-$(function(){ // czekanie na załadowanie DOM
-  var carouselList = $("#carousel ul"); // pobieramy listę elementów i przypisujemy do zmiennej
-	/*var timeChangeSlides = */setInterval(function(){ // funkcja 'setInterval' jest wykonywana co pewien okres czasu (a nie tylko raz po pewnym okresie czasu).
-		carouselList.animate({marginLeft:-800}, 2000, function(){ // animacja/właściwości css animowanego elementu/czas animacji/funkcja która wykona się na końcu animacji ('callback') = animujemy wartość lewego marginesu.
-			var firstItem = carouselList.find("li:first");
+$(function() {
+  var carouselList = $("#carousel ul");
+  setInterval(function() {
+    carouselList.animate({
+      marginLeft: -800
+    }, 2000, function() {
+      var firstItem = carouselList.find("li:first");
       var lastItem = carouselList.find("li:last");
-        lastItem.after(firstItem);
-      // $(this).find("li:last").after($(this).find("li:first")); - również działa zamiast :6 :7 :8
-			  carouselList.css({marginLeft:0});
-		});
+      lastItem.after(firstItem);
+      carouselList.css({
+        marginLeft: 0
+      });
+    });
   });
 });
